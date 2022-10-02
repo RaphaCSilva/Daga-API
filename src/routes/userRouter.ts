63847e9ee4d6fd35f3c9194847973cb0352e08b8
Raphael <raphaelcavalcantesilva1999@gmail.com>
2022-10-02 09:30:58 -0300

    feat: add userController file

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { createUser, login } from "../controller/userController";
 import { validateSchemaMiddleware } from "../middlewares/validateSchema";
 import { createUserSchema } from "../schemas/createUserSchema";
 import { loginSchema } from "../schemas/loginSchema";
 
 const userRouter = Router();
 
-userRouter.post('/sign-up', validateSchemaMiddleware(createUserSchema));
-userRouter.post('/sign-in', validateSchemaMiddleware(loginSchema));
+userRouter.post('/sign-up', validateSchemaMiddleware(createUserSchema), createUser);
+userRouter.post('/sign-in', validateSchemaMiddleware(loginSchema), login);
 
 export default userRouter;
