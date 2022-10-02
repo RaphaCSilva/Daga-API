05aa34d4982af832c30c0add9c58b3dc7e429d39
Raphael <raphaelcavalcantesilva1999@gmail.com>
2022-10-02 09:13:03 -0300

    feat: add error handler and router to server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import cors from 'cors';
 import express, {json} from 'express';
 import 'express-async-errors';
+import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware';
+import router from './routes/index';
 
 const app = express();
 
 app.use(cors());
 app.use(json());
+app.use(router);
+app.use(errorHandlerMiddleware);
+
 
 export default app;
