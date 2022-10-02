d8b5984db3581d91cb04549e0478becfab3b88b1
Raphael <raphaelcavalcantesilva1999@gmail.com>
2022-10-02 09:27:07 -0300

    feat: add validateSchema to server

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import { validateSchemaMiddleware } from "../middlewares/validateSchema";
+import { createUserSchema } from "../schemas/createUserSchema";
+import { loginSchema } from "../schemas/loginSchema";
 
 const userRouter = Router();
 
-userRouter.post('/sign-up');
-userRouter.post('/sign-in');
+userRouter.post('/sign-up', validateSchemaMiddleware(createUserSchema));
+userRouter.post('/sign-in', validateSchemaMiddleware(loginSchema));
 
 export default userRouter;
