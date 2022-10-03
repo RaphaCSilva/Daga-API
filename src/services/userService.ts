0854fe81243febaf3f7e15ced72de5267031ffaa
Raphael <raphaelcavalcantesilva1999@gmail.com>
2022-10-03 06:04:04 -0300

    feat: sign-up completed

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,9 +1,29 @@
 import { CreateUserData } from "../repositories/userRepository";
+import * as userRepository from "../repositories/userRepository";
+import bcrypt from "bcrypt";
+import { conflictError } from "../utils/errorUtils";
 
 export async function signup(user: CreateUserData) {
+    await verifyEmailAlreadyRegistered(user.email);
     
+    const SALT = 10;
+    const encyptedPassword = bcrypt.hashSync(user.password, SALT);
+    const data {
+        email: user.email,
+        password: encyptedPassword
+    }
+
+    await userRepository.insertUser(data);
 }
 
 export async function signin(user: CreateUserData) {
 
+}
+
+async function verifyEmailAlreadyRegistered(email: string) {
+    const emailRegistered = userRepository.findUserByEmail(email);
+
+    if(emailRegistered) {
+        return conflictError('Email already registered');
+    }
 }
